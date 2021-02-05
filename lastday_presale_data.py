7d9ca5d1309f6a0bde25fa2ebc0b7b0c788bee9c
akari <1647522256@qq.com>
2021-02-05 18:51:36 +0800

    data reset index

--- lastday_presale_data.py
+++ lastday_presale_data.py
@@ -92,8 +92,7 @@ def reorder_df(df,field_list,field):
         each_df = df[df[field].isin([each_field])]
         #按场次降序
         each_df = each_df.sort_values(by = "场次",ascending = False,inplace = True)
-        if len(each_df) != 0:
-            each_df = pd.DataFrame(each_df.reset_index(drop = True))
+        each.reset_index(drop = True,inplace = True)
         df_total = pd.concat([df_total,each_df],ignore_index = True)
         
     return df_total
@@ -170,9 +169,8 @@ def pivot_data(df,presale_date,fetch_date):
                 df_table["上座率"] = np.round((df_table["人次"] / df_table["总座位数"] * 100),2)
                 df_table["场均人次"] = np.round((df_table["人次"] /df_table["场次"]),2)
                 df_table["平均票价"] = np.round((df_table["票房"] / df_table["人次"]),2)
-                df_table = df_table.sort_values(by = "场次",ascending = False,inplace = True)
-                if len(df_table) != 0:
-                    df_table = pd.DataFrame(df_table.reset_index(drop = True))
+                df_table.sort_values(by = "场次",ascending = False,inplace = True)
+                df_table.reset_index(drop = True,inplace = True)
             
             df_table["预售日期"] = str(presale_date)
             df_table["获取日期"] = str(fetch_date)
