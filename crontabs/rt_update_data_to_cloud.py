96c8f200ce09e58dc27e692a3f793309f4520f1d
akari <1647522256@qq.com>
2022-01-21 19:06:08 +0800

    update data to cloud

---
+++ crontabs/rt_update_data_to_cloud.py
@@ -0,0 +1,82 @@
+from sshtunnel import SSHTunnelForwarder
+import pandas as pd
+import datetime
+import pymysql
+from sqlalchemy import create_engine
+from logger import get_logger
+
+today = datetime.date.today()
+yesterday = str(today - datetime.timedelta(days = 1))
+#判断专资时间
+deal_date = str(datetime.datetime.today() - datetime.timedelta(hours = 6))[:10]
+
+def ssh_port():
+    server = SSHTunnelForwarder(
+    ssh_address_or_host = ("1.12.243.7",22),
+    ssh_username = "root",
+    ssh_password = "JinYi2017*",
+    remote_bind_address =  ("localhost",3306))
+    server.start()
+
+    return server,server.local_bind_port
+
+#可以通过port定义ssh连接
+def connect_to_db(port = 3306):
+    conn = pymysql.connect(
+        user = "root",
+        passwd = "jy123456",
+        host = "localhost",
+        db = "film_data",
+        port = port)
+
+    return conn
+
+def update_data_to_ssh(df,tablename,port):
+    conn = create_engine("mysql+pymysql://root:jy123456@127.0.0.1:%s/film_data?charset=utf8" % port)
+    df.to_sql(tablename,con = conn,if_exists = "append",index = False)
+
+def get_df(sql,conn):
+    res = pd.read_sql(sql,conn)
+    df = pd.DataFrame(res)
+    return df
+
+if __name__ == "__main__":
+    server,server_port = ssh_port()
+    conn_local = connect_to_db()
+    conn_server = connect_to_db(port = server_port)
+    cursor_server = conn_server.cursor()
+    logger = get_logger("/home/log/film_data","update_ssh_data")
+    #若为6-24点
+    if deal_date == str(today):
+        #获取本地当日数据
+        df_film_cinema_sql = "select * from film_cinema where fetch_date = '%s' and op_date >= '%s'" % (str(today),str(today))
+        df_presale_film_cinema_sql = "select * from presale_film_cinema where fetch_date = '%s'" % str(today)
+        df_film_cinema = get_df(df_film_cinema_sql,conn_local)
+        df_film_cinema.drop(columns = ["id"],axis  =1,inplace = True)
+        df_presale_film_cinema = get_df(df_presale_film_cinema_sql,conn_local)
+        df_presale_film_cinema.drop(columns = ["id"],axis = 1,inplace = True)
+        #清除远程当日数据   
+        cursor_server.execute("delete from film_cinema where fetch_date = '%s' and op_date >= '%s'" % (str(today),str(today)))
+        cursor_server.execute("delete from presale_film_cinema where fetch_date = '%s'" % str(today))
+        #向远程写入当日数据
+        update_data_to_ssh(df_film_cinema,"film_cinema",server_port)
+        update_data_to_ssh(df_presale_film_cinema,"presale_film_cinema",server_port)
+
+    elif deal_date == yesterday:
+        #获取本地当日数据
+        df_film_cinema_sql = "select * from film_cinema where fetch_date in ('%s','%s') and op_date >= '%s'" % (yesterday,str(today),str(today))
+        df_presale_film_cinema_sql = "select * from presale_film_cinema where fetch_date = '%s'" % yesterday
+        df_film_cinema = get_df(df_film_cinema_sql,conn_local)
+        df_film_cinema.drop(columns = ["id"],axis  =1,inplace = True)
+        df_presale_film_cinema = get_df(df_presale_film_cinema_sql,conn_local)
+        df_presale_film_cinema.drop(columns = ["id"],axis = 1,inplace = True)
+        #清除远程当日数据
+        cursor_server.execute("delete from film_cinema where fetch_date in ('%s','%s') and op_date >= '%s'" % (yesterday,str(today),str(today)))
+        cursor_server.execute("delete from presale_film_cinema where fetch_date = '%s'" % yesterday)
+        #向远程写入当日数据
+        update_data_to_ssh(df_film_cinema,"film_cinema",server_port)
+        update_data_to_ssh(df_presale_film_cinema,"presale_film_cinema",server_port)
+
+    server.stop()
+    print("update data to ssh success")
+    logger.info("update data to ssh success")
